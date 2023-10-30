1ff1848d1db74492449c2b38736622921e991305
Joun <urbanscratcher@gmail.com>
2023-10-30 16:27:16 +0900

    search bar done

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 from ytmusicapi import YTMusic
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 ytmusic = YTMusic('oauth.json')
 app = FastAPI()
+
+origins = [
+    'http://localhost:5173',
+    'http://144.24.84.244:5173'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 @app.get('/search')
 def search(q):    
